5da76e1738d929521b6dc25f9d1d7d574ec477dd
gianni28 <a.gianni2003@gmail.com>
2024-10-10 15:12:02 -0500

    se soluciona problema routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom"; 
+import { Route, Routes } from "react-router-dom"; 
 import Home from "./pages/home";
 import DetailMeal from "./pages/DetailMeal/index"; 
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/meal/:idMeal" element={<DetailMeal />} /> 
       </Routes>
     </div>
-    </Router>
   );
 }
 
