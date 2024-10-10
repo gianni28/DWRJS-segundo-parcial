f191c7ddc7df41d44ae9e2d1042849ef41c0e353
gianni28 <a.gianni2003@gmail.com>
2024-10-10 10:56:41 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom"; 
+import { Route, Router, Routes } from "react-router-dom"; 
 import Home from "./pages/home";
 import DetailMeal from "./pages/DetailMeal/index"; 
 
 function App() {
   return (
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/meal/:idMeal" element={<DetailMeal />} /> 
       </Routes>
     </div>
+    </Router>
   );
 }
 
